41071a76b817a7dd848bf01ca59bc819aa5fe8fe
rahul5403 <rahul54meshram@gmail.com>
2024-12-23 18:17:16 +0530

    finallyy updated url's

--- backend/middlewares/isAuthenticated.js
+++ backend/middlewares/isAuthenticated.js
@@ -5,14 +5,14 @@ const isAuthenticated = async (req, res, next) => {
         const token = req.cookies.token;
         if (!token) {
             return res.status(401).json({
-                message: "User not authenticated",
+                message: "User not authenticatedddd",
                 success: false,
             })
         }
         const decode = await jwt.verify(token, process.env.SECRET_KEY);
         if(!decode){
             return res.status(401).json({
-                message:"Invalid token",
+                message:"Invalid tokennnn",
                 success:false
             })
         };
--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -4,8 +4,7 @@
 // export const COMPANY_API_END_POINT="http://localhost:5000/api/v1/company";
 
 
-
-export const USER_API_END_POINT =  "/api/v1/user";
+export const USER_API_END_POINT = "/api/v1/user";
 export const JOB_API_END_POINT = "/api/v1/job";
 export const APPLICATION_API_END_POINT = "/api/v1/application";
 export const COMPANY_API_END_POINT = "/api/v1/company";
