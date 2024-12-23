aa1b2663701f83915066e6552f150006a06e102b
rahul5403 <rahul54meshram@gmail.com>
2024-12-23 10:12:10 +0530

    cors updated

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-// import cors from "cors";
+import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
@@ -14,11 +14,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
-// const corsOptions = {
-//     origin:'http://localhost:5173',
-//     credentials:true
-// }
-// app.use(cors(corsOptions));
+const corsOptions = {
+    origin:'http://localhost:5173',
+    credentials:true
+}
+app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 8000;
 
